fc22fb7f4062853aed42fafd3874035d3d6b8a0c
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-16 10:50:08 +0700

    feat: delete product

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,8 @@
 "use server";
 import { prisma } from "@/db/prisma";
-import { convertToPlainObject } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
+import { revalidatePath } from "next/cache";
 
 // Get latest product
 export async function getLatestProducts() {
@@ -34,13 +35,40 @@ export async function getAllProducts({
 }) {
   const data = await prisma.product.findMany({
     skip: (page - 1) * limit,
-    take: limit
-  })
+    take: limit,
+  });
 
-  const dataCount = await prisma.product.count()
+  const dataCount = await prisma.product.count();
 
   return {
     data,
-    totalPages: Math.ceil(dataCount / limit)
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+// Delete a product
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id },
+    });
+
+    if (!productExists) throw new Error("Product not found");
+
+    await prisma.product.delete({
+      where: { id },
+    });
+
+    revalidatePath("/admin/products");
+
+    return {
+      success: true,
+      message: "Product deleted successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
   }
 }
