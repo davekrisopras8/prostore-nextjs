53cf2f95704d604427ac850698923543fdd5a5d1
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-18 15:33:07 +0700

    feat: admin order search and admin user search

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -2,25 +2,45 @@ import DeleteDialog from "@/components/shared/delete-dialog";
 import Pagination from "@/components/shared/pagination";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { deleteUser, getAllUsers } from "@/lib/actions/user.actions";
 import { formatUUID } from "@/lib/utils";
 import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Admin Users'
-}
+  title: "Admin Users",
+};
 
-const AdminUserPage = async( props: {searchParams: Promise<{page: string}>}) => {
-  const {page = '1'} = await props.searchParams
+const AdminUserPage = async (props: {
+  searchParams: Promise<{ page: string; query: string }>;
+}) => {
+  const { page = "1", query: searchText } = await props.searchParams;
 
-  const users = await getAllUsers({page: Number(page)})
-  console.log(users)
+  const users = await getAllUsers({ page: Number(page), query: searchText });
 
-    return (
+  return (
     <div className="space-y-2">
-      <h2 className="h2-bold">Users</h2>
+      <div className="flex items-center gap-3">
+        <h1 className="h2-bold">Users</h1>
+        {searchText && (
+          <div>
+            Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+            <Link href="/admin/users">
+              <Button variant="outline" size="sm">
+                Remove Filter
+              </Button>
+            </Link>
+          </div>
+        )}
+      </div>
       <div className="overflow-x-auto">
         <Table>
           <TableHeader>
@@ -36,12 +56,14 @@ const AdminUserPage = async( props: {searchParams: Promise<{page: string}>}) => 
             {users.data.map((user) => (
               <TableRow key={user.id}>
                 <TableCell>{formatUUID(user.id)}</TableCell>
-                <TableCell>
-                  {user.name}
-                </TableCell>
+                <TableCell>{user.name}</TableCell>
                 <TableCell>{user.email}</TableCell>
                 <TableCell>
-                  {user.role === 'user' ? (<Badge variant='secondary'>User</Badge>) : (<Badge variant='default'>Admin</Badge>)}
+                  {user.role === "user" ? (
+                    <Badge variant="secondary">User</Badge>
+                  ) : (
+                    <Badge variant="default">Admin</Badge>
+                  )}
                 </TableCell>
                 <TableCell>
                   <Button asChild variant="outline" size="sm">
@@ -54,14 +76,11 @@ const AdminUserPage = async( props: {searchParams: Promise<{page: string}>}) => 
           </TableBody>
         </Table>
         {users.totalPages > 1 && (
-          <Pagination
-            page={Number(page) || 1}
-            totalPages={users?.totalPages}
-          />
+          <Pagination page={Number(page) || 1} totalPages={users?.totalPages} />
         )}
       </div>
     </div>
   );
-}
+};
 
 export default AdminUserPage;
