e71cda4e1d22490d4453daaac25c3697c2ae3cad
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-21 10:08:51 +0700

    feat: featured product components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,20 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
-const HomePage = async() => {
-  const latestProducts = await getLatestProducts()
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
-      <ProductList
-        data={latestProducts}
-        title="Newest Arrivals"
-      />
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
 };
