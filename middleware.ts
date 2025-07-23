325eca0b437f452b4f9c3368daf0a1f378148b2d
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-23 11:36:21 +0700

    fix: edge functions middleware size 1mb

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,49 @@
-import NextAuth from "next-auth";
-import { config } from "@/auth";
+import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export const { auth: middleware } = NextAuth(config);
+export default withAuth(
+  function middleware(req: NextRequest) {
+    // Contoh: generate sessionCartId jika belum ada
+    if (!req.cookies.get("sessionCartId")) {
+      const response = NextResponse.next();
+      response.cookies.set("sessionCartId", crypto.randomUUID());
+      return response;
+    }
+
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }: { token: any; req: NextRequest }) => {
+        const protectedPaths = [
+          /\/shipping-address/,
+          /\/payment-method/,
+          /\/place-order/,
+          /\/profile/,
+          /\/user\/(.*)/,
+          /\/order\/(.*)/,
+          /\/admin/,
+        ];
+
+        const { pathname } = req.nextUrl;
+        if (!token && protectedPaths.some((r) => r.test(pathname))) {
+          return false;
+        }
+
+        return true;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: [
+    "/shipping-address",
+    "/payment-method",
+    "/place-order",
+    "/profile",
+    "/user/:path*",
+    "/order/:path*",
+    "/admin/:path*",
+  ],
+};
