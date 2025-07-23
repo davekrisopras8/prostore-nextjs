74d3ec4835229477397a4a4ad6cf8c3f21d3f92e
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-23 11:27:38 +0700

    fix: config auth

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import { config } from "@/auth";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("auth_token")?.value;
-  if (!token) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/admin/:path*", "/user/:path*"],
-  runtime: "nodejs",
-};
+export const { auth: middleware } = NextAuth(config);
