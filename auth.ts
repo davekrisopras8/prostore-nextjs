5928b6e96dc5e601210ad240136762635ff4ee98
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-23 13:25:06 +0700

    tes prod 3

--- auth.ts
+++ auth.ts
@@ -1,26 +1,26 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/db/prisma";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { compareSync } from "bcrypt-ts-edge";
-import type { NextAuthConfig } from "next-auth";
-import { cookies } from "next/headers";
+import NextAuth from 'next-auth';
+import { authConfig } from '@/auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/db/prisma';
+import { cookies } from 'next/headers';
+import { compare } from '@/lib/encrypt';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-export const authConfig = {
+export const { handlers, auth, signIn, signOut } = NextAuth({
   pages: {
-    signIn: "/sign-in",
-    error: "/sign-in",
+    signIn: '/sign-in',
+    error: '/sign-in',
   },
   session: {
-    strategy: "jwt",
+    strategy: 'jwt' as const,
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       credentials: {
-        email: { type: "email" },
-        password: { type: "password" },
+        email: { type: 'email' },
+        password: { type: 'password' },
       },
       async authorize(credentials) {
         if (credentials == null) return null;
@@ -34,7 +34,7 @@ export const authConfig = {
 
         // Check if user exists and if the password matches
         if (user && user.password) {
-          const isMatch = compareSync(
+          const isMatch = await compare(
             credentials.password as string,
             user.password
           );
@@ -49,47 +49,46 @@ export const authConfig = {
             };
           }
         }
-
-        // If user does not exists or password does not match return null
+        // If user does not exist or password does not match return null
         return null;
       },
     }),
   ],
-
   callbacks: {
-    async session({ session, user, trigger, token }: any) {
+    ...authConfig.callbacks,
+    async session({ session, user, trigger, token }) {
       // Set the user ID from the token
       session.user.id = token.sub;
       session.user.role = token.role;
       session.user.name = token.name;
 
       // If there is an update, set the user name
-      if (trigger === "update") {
+      if (trigger === 'update') {
         session.user.name = user.name;
       }
 
       return session;
     },
-    async jwt({ token, user, trigger, session }: any) {
+    async jwt({ token, user, trigger, session }) {
       // Assign user fields to token
       if (user) {
         token.id = user.id;
         token.role = user.role;
 
         // If user has no name then use the email
-        if (user.name === "NO_NAME") {
-          token.name = user.email!.split("@")[0];
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
 
-          // Update the database to reflect the token name
+          // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
             data: { name: token.name },
           });
         }
 
-        if (trigger === "signIn" || trigger === "signUp") {
+        if (trigger === 'signIn' || trigger === 'signUp') {
           const cookiesObject = await cookies();
-          const sessionCartId = cookiesObject.get("sessionCartId")?.value;
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
 
           if (sessionCartId) {
             const sessionCart = await prisma.cart.findFirst({
@@ -113,13 +112,13 @@ export const authConfig = {
       }
 
       // Handle session updates
-      if (session?.user.name && trigger === "update") {
+      if (session?.user.name && trigger === 'update') {
         token.name = session.user.name;
       }
 
       return token;
     },
   },
-} satisfies NextAuthConfig;
+});
+export { authConfig };
 
-export const { handlers, auth, signIn, signOut } = NextAuth(authConfig);
