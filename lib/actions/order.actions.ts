d59eb50c16f49e796344c1ae9fdbaabef9ac6f8e
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-07-24 11:30:01 +0700

    feat: webhooks stripe to make update order to paid

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -211,7 +211,7 @@ export async function approvePaypalOrder(
 }
 
 // Update order to paid
-async function updateOrderToPaid({
+export async function updateOrderToPaid({
   orderId,
   paymentResult,
 }: {
